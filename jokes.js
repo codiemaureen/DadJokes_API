51e6378d11cd4b93749c6bc8d2a5520c418ca126
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-02 12:44:46 -0500

    Linking button

--- jokes.js
+++ jokes.js
@@ -11,3 +11,18 @@ const buttonText = [
     'please stop',
     'That was the worst',
 ];
+
+async function fetchJoke(){
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers:{
+            Accept: 'application/json',
+        },
+    });
+    const joke = await response.json();
+    console.log(joke);
+};
+
+async function handleClick(){
+    const joke = await fetchJoke();
+}
+
