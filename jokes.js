7565055a064ca4c47ce8c8444834daebde60b129
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-02 13:11:23 -0500

    Adding loader

--- jokes.js
+++ jokes.js
@@ -1,6 +1,6 @@
 const jokeButton = document.querySelector('.getJoke');
 const jokeHolder = document.querySelector('.joke p');
-
+const loader = document.querySelector('.loader');
 
 const buttonText = [
     'Ugh',
@@ -13,18 +13,38 @@ const buttonText = [
 ];
 
 async function fetchJoke(){
+    //turn loader on
+    loader.classList.remove('hidden');
+    //hide button while loading
+    jokeButton.classList.add('hidden')
     const response = await fetch('https://icanhazdadjoke.com', {
         headers:{
             Accept: 'application/json',
         },
     });
-    const joke = await response.json();
-    console.log(joke);
+    const data = await response.json();
+    //turn loader off
+    loader.classList.add('hidden');
+    jokeButton.classList.remove('hidden')
+    return data;
 };
 
+function randomItemFromArray(arr, not){
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    if(item === not){
+        console.log('We used that one last time!');
+        return randomItemFromArray(arr,not);
+    };
+    return item;
+}
+
 async function handleClick(){
-    const joke = await fetchJoke();
-    console.log(joke);
+    const { joke } = await fetchJoke();
+    jokeHolder.textContent = joke;
+    jokeButton.textContent = randomItemFromArray(
+        buttonText,
+        jokeButton.textContent
+    );
 }
 
 jokeButton.addEventListener('click', handleClick);
