b589e20a25b4e905816ede2a8f1e1989691c01be
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-02 12:33:56 -0500

    Initial Setup

---
+++ jokes.js
@@ -0,0 +1,13 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
+
+const buttonText = [
+    'Ugh',
+    '🤦🏼‍♀️',
+    'Omg dad.',
+    'seriously',
+    'stop it.',
+    'please stop',
+    'That was the worst',
+];
